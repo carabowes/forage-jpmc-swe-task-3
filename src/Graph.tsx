17335ed46d08599ff17346386f4c6417b610dcc1
“Cara <2757542b@student.gla.ac.uk>
2024-08-30 16:05:16 +0100

    Implemented graph for task 3

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -11,6 +11,7 @@ interface IProps {
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
+
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
@@ -19,30 +20,34 @@ class Graph extends Component<IProps, {}> {
   }
 
   componentDidMount() {
-    // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
       timestamp: 'date',
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "upper_bound", "lower_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
         timestamp: 'distinct count',
       }));
     }
@@ -51,10 +56,77 @@ class Graph extends Component<IProps, {}> {
   componentDidUpdate() {
     if (this.table) {
       this.table.update(
-        DataManipulator.generateRow(this.props.data),
+        DataManipulator.generateRow(this.props.data) as unknown as any
       );
     }
   }
 }
 
 export default Graph;
+
+// import React, { Component } from 'react';
+// import {Table, TableData} from '@finos/perspective';
+// import { ServerRespond } from './DataStreamer';
+// import { DataManipulator } from './DataManipulator';
+// import './Graph.css';
+//
+// interface IProps {
+//   data: ServerRespond[],
+// }
+//
+// interface PerspectiveViewerElement extends HTMLElement {
+//   load: (table: Table) => void,
+// }
+// class Graph extends Component<IProps, {}> {
+//   table: Table | undefined;
+//
+//   render() {
+//     return React.createElement('perspective-viewer');
+//   }
+//
+//   componentDidMount() {
+//     // Get element from the DOM.
+//     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+//
+//     const schema = {
+//       price_abc: 'float',
+//       price_def: 'float',
+//       ratio: 'float',
+//       timestamp:'date',
+//       upper_bound:'float',
+//       lower_bound:'float',
+//       trigger_alert:'float',
+//
+//     };
+//
+//     if (window.perspective && window.perspective.worker()) {
+//       this.table = window.perspective.worker().table(schema);
+//     }
+//     if (this.table) {
+//       // Load the `table` in the `<perspective-viewer>` DOM reference.
+//       elem.load(this.table);
+//       elem.setAttribute('view', 'y_line');
+//       elem.setAttribute('row-pivots', '["timestamp"]');
+//       elem.setAttribute('columns', '["ratio","lower_bound","upper_bound","trigger_alert"]');
+//       elem.setAttribute('aggregates', JSON.stringify({
+//       price_abc: 'float',
+//       price_def: 'float',
+//       ratio: 'float',
+//       timestamp:'date',
+//       upper_bound:'float',
+//       lower_bound:'float',
+//       trigger_alert:'float',
+//       }));
+//     }
+//   }
+//
+// componentDidUpdate() {
+//   if (this.table) {
+//     const rowData: TableData = [DataManipulator.generateRow(this.props.data)];
+//     console.log('Updating Table with Data:', rowData);
+//     this.table.update(rowData);
+//   }
+// }
+// }
+//
+// export default Graph;
